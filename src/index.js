43a8acf1bb84ed9b87d0b30dbdad4ece2eb10c75
Geoff Vincent <geoffkvincent@gmail.com>
2018-10-15 12:04:25 -0600

    flash update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const FlashContainer = styled.div`
   width: 100%;
   margin: 0 auto;
-  paddding: 15px:
+  padding: 15px;
   margin-bottom: 20px !important;
   border: 1px solid transparent;
   border-radius: 4px;
@@ -13,21 +13,20 @@ const FlashContainer = styled.div`
 const FlashError = styled(FlashContainer)`
   color: #A94442;
   background-color: #F2DEDE;
-  borderColor: #EBCCD1;
+  border-color: #EBCCD1;
 `
 
-const FlashSucces = styled(FLashContainer)`
+const FlashSuccess = styled(FlashContainer)`
   color: #3C763D;
   background-color: #DFF0D8;
   border-color: #D6E9C6;
 `
 
-const FlashInfo = styled(FlashCOntainer)`
+const  FlashInfo = styled(FlashContainer)`
   color: #31708F;
-  background-color: #D9EDf7;
+  background-color: #D9EDF7;
   border-color: #BCE8F1;
 `
-
 const Close = styled.span`
   float: right;
   color: grey;
@@ -35,30 +34,26 @@ const Close = styled.span`
   padding-right: 50px;
 `
 
-
-
 class Flash extends React.Component {
   state = {...this.props}
-  
+
   componentDidUpdate(prevProps) {
-    if (prevProps !== this.props )
+    if (prevProps !== this.props)
       this.setState({...this.props})
   }
 
   fade = () => {
-    setTimeout ( () => {
-      this.clearFlash()
-    }, this.props.duration || 10000) 
+    setTimeout( this.clearFlash, this.props.duration || 10000)
   }
 
   clearFlash = () => {
-    this.setState({message: '', msgType: ''})
+    this.setState({ message: '', msgType: '' })
   }
 
   render() {
     let FlashComponent
 
-    const {message, msgType } = this.state
+    const { message, msgType } = this.state
     if (message) {
       switch(msgType) {
         case 'success':
@@ -68,18 +63,18 @@ class Flash extends React.Component {
           FlashComponent = FlashError
           break
         default:
-          FlashComponent = flashInfo
+          FlashComponent = FlashInfo
       }
 
-    return(
-      <FlashComponent>
-        {message}
-        { this.fade() }
-        <Close onClick={this.clearflash}>close</Close>
-      </FlashComponent>
-    )
+      return (
+        <FlashComponent>
+          { message }
+          { this.fade() }
+          <Close onClick={this.clearFlash}>close</Close>
+        </FlashComponent>
+      )
     } else {
-     return null
+      return null
     }
   }
 }
